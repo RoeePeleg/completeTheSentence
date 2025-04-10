0f295542b78cfb8ca6b07077861c7383577f7586
RoeePeleg <pelegroee@gmail.com>
2025-04-09 20:22:48 -0700

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const GAME_CONFIG = {
+  timeLimitInSeconds: 60,
+  numberOfPlayers: 3,
+  sentences: [
+    "I once saw [person] doing something really funny...",
+    "If [person] were a superhero, their superpower would be...",
+    "The weirdest thing [person] ever said was...",
+    "If [person] was an animal, they would be a [animal] because...",
+    "The funniest memory with [person] is when...",
+  ],
+};
